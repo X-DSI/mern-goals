16be7a23ee6a4dfe8bad298a8dc51e6ecfd8b481
X-DSI <xmandsi@gmail.com>
2022-08-31 05:14:54 +0530

    Added Eslint and Prettier config files

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,31 @@
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
+
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    cpassword: '',
+  })
+
+  const {name, email, password, cpassword} = formData
+  
   return (
-    <div>Register</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account here</p>
+      </section>
+      <section className="form">
+        <form>
+          <input type="text" className="form-control" id='name' name='name' value={name} placeholder='What are you called?' onChange={onChange} />
+        </form>
+      </section>
+    </>
   )
 }
 
-export default Register
+export default Register 
