c41afb380cbf07e469f2f890f9c58fd85dd181d0
X-DSI <xmandsi@gmail.com>
2022-08-31 10:27:04 +0530

    Done Register Page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,15 @@ function Register() {
     cpassword: '',
   })
 
-  const onChange = () => {}
+  const onChange = e => {
+    setFormData(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+  const onSubmit = e => {
+    e.preventDefault()
+  }
 
   const { name, email, password, cpassword } = formData
 
@@ -22,16 +30,56 @@ function Register() {
         <p>Please create an account here</p>
       </section>
       <section className="form">
-        <form>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={name}
-            placeholder="What are you called?"
-            onChange={onChange}
-          />
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="What are you called?"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="What's your email address?"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="What do you want as a password?"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="cpassword"
+              name="cpassword"
+              value={cpassword}
+              placeholder="Retype your password to confirm it!"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
         </form>
       </section>
     </>
