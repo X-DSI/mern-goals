b44213c73722b1a8b3d85c37287b85d03159382f
X-DSI <xmandsi@gmail.com>
2022-09-05 12:41:46 +0530

    Update Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+
+  return <div>Dashboard</div>
 }
 
 export default Dashboard
