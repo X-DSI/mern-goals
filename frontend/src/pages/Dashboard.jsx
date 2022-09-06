05d962bfdeb6b7834ea98bde5212c0f554101246
X-DSI <xmandsi@gmail.com>
2022-09-06 20:09:09 +0530

    Issue with useEffect

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,25 +1,45 @@
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
+// import GoalItem from '../components/GoalItem'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const { user } = useSelector(state => state.auth)
+  const { goals, isLoading, isError, message } = useSelector(
+    state => state.goals
+  )
 
   useEffect(() => {
+    if (isError) {
+      console.log(message)
+    }
+
     if (!user) {
       navigate('/login')
     }
-  }, [user, navigate])
+
+    dispatch(getGoals())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
       <section className="heading">
         <h1>Welcome {user && user.name}</h1>
-        <p>Your goals are waiting for you!</p>
+        <p>Goals Dashboard</p>
       </section>
 
       <GoalForm />
